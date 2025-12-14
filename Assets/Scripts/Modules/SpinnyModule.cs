17086da952ca444e72819a679a04976d329cd076
Hidd3n-47 <122316898+Hidd3n-47@users.noreply.github.com>
2025-12-14 04:06:24 +0000

    Spinny module start

---
+++ Assets/Scripts/Modules/SpinnyModule.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SpinnyModule : IModule
+{
+    [SerializeField]
+    private Transform mNeedle;
+
+    [SerializeField]
+    private Transform mValve;
+
+    private void Update()
+    {
+
+    }
+}
