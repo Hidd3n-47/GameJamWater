992871dc30eb72833c1946b8c2eaae13e6658084
Hidd3n-47 <122316898+Hidd3n-47@users.noreply.github.com>
2025-12-14 20:56:11 +0000

    clock

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -7,17 +7,22 @@ public class Clock : MonoBehaviour
     private GameManager mGameManager;
 
     [SerializeField]
+    private Transform mHourHand;
+    [SerializeField]
     private Transform mMinuteHand;
+
+    void Start()
+    {
+        mHourHand.localRotation = Quaternion.Euler(0.0f, 0.0f, mGameManager.mStartShiftTime / 12.0f * 360.0f);
+    }
 
     void Update()
     {
-
         float time = math.lerp(mGameManager.mStartShiftTime, mGameManager.mEndShiftTime, mGameManager.mDayTimer / mGameManager.mTotalTimeForDay);
 
         int minutes = (int)((time - (int)time) * 60.0f);
 
-        mMinuteHand.eulerAngles = new Vector3(0.0f, 0.0f, minutes / 60.0f);
-
-        //mClockTimer.text = ((int)time) + ":" + (minutes < 10 ? "0" + minutes : minutes);
+        mHourHand.localRotation = Quaternion.Euler(0.0f, 0.0f, time / 12.0f * 360.0f);
+        mMinuteHand.localRotation = Quaternion.Euler(0.0f, 0.0f, (time - (int)time) * 360.0f);
     }
 }
